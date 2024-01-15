4130877b5d02bf16a6a48c4ed16a4ca193342d56
Alice LIN <alilin@bess-f2r2s14.clusters.42paris.fr>
2024-01-15 19:38:56 +0100

    enabled root check now that we have the container env set up

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,4 +1,3 @@
-//#include "Tintin_reporter.hpp"
 #include "Matt_daemon.hpp"
 #include <sys/stat.h>
 
@@ -16,13 +15,11 @@ bool	daemon_running(const std::string& filename) {
 
 int main()
 {
-	// if (getuid() != 0)
-	// {
-	// 	std::cout << "User launching the daemon server is not root" << std::endl;
-	// 	return (1);
-	// }
-	//check matt_daemon.lock doeas not exist
-	// Create a daemon
+	if (getuid() != 0)
+	{
+		std::cout << "User launching the daemon server is not root" << std::endl;
+		return (1);
+	}
 	if (daemon_running("/var/lock/matt_daemon.lock")) {
 		std::cout << "Matt_daemon is already running" << std::endl;
 		std::cout << "use : ps aux | grep Matt_daemon to figure out the PID process" << std::endl;
